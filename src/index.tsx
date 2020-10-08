3f8d21a9a937f8fc0d723c1f011255bf9e2cac4a
Charles Allen <charles@courtHive.com>
2020-10-08 08:16:40 +0200

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,1 @@
 export { EliminationStructure } from './EliminationStructure';
-
-/**
- * A Component to generate draws
-export const DrawGridStructure = props => {
-  return (
-    <div>
-      <EliminationStructure {...props} />
-    </div>
-  );
-};
- */
