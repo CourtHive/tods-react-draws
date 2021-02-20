4ae25817d7078438e399f1a3d098b99923af36c7
CourtHive <charles@courthive.com>
2021-02-20 20:00:41 +0100

    updates

--- src/styles/gridStyles.js
+++ src/styles/gridStyles.js
@@ -1,39 +1,39 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useStyles = makeStyles((theme) => ({
+export const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(2),
-    marginBottom: theme.spacing(3)
+    marginBottom: theme.spacing(3),
   },
   drawRoot: {
     width: '100%',
-    flexWrap: 'nowrap'
+    flexWrap: 'nowrap',
   },
   connectorColumn: {
     minWidth: 15,
-    maxWidth: 20
+    maxWidth: 20,
   },
   detailsColumn: {
-    flexWrap: 'nowrap'
+    flexWrap: 'nowrap',
   },
   initialColumn: {
     minWidth: 200,
-    maxWidth: 250
+    maxWidth: 250,
   },
   roundColumn: {
     minWidth: 100,
-    maxWidth: 110
+    maxWidth: 110,
   },
   participant: {
     paddingLeft: 3,
     paddingRight: 3,
     '&:hover': {
       cursor: 'pointer',
-      backgroundColor: '#f5f5f5'
-    }
+      backgroundColor: '#f5f5f5',
+    },
   },
   score: {
-    paddingLeft: 3
+    paddingLeft: 3,
   },
   roundName: {
     marginLeft: 3,
@@ -41,27 +41,27 @@ export const useStyles = makeStyles((theme) => ({
     height: '1.5em',
   },
   noBoderBottom: {
-    paddingBottom: '2px'
+    paddingBottom: '2px',
   },
   thickBorderBottom: {
-    borderBottom: '2px solid black'
+    borderBottom: '2px solid black',
   },
   verticalDivider: {
     width: 1,
     marginTop: '4em',
-    borderRight: '1px solid black'
+    borderRight: '1px solid black',
   },
   borderRight: {
-    borderRight: '1px solid black'
+    borderRight: '1px solid black',
   },
   borderBottom: {
-    borderBottom: '1px solid black'
+    borderBottom: '1px solid black',
   },
   bracketTop: {
-    borderBottom: '1px solid black'
+    borderBottom: '1px solid black',
   },
   bracketBottom: {
-    borderBottom: '1px solid black'
+    borderBottom: '1px solid black',
   },
-  bracketContent: {}
+  bracketContent: {},
 }));
